00658684e5a17ddd1dfc85a641f605f479f9926f
Przemek <przemek12wiewiora@gmail.com>
2025-07-07 22:48:55 +0200

    add simple counter

---
+++ RPI_PICO_W/lcd_i2c_simple_counter/simple_counter.ino
@@ -0,0 +1,44 @@
+// This code requires "LiquidCrystal I2C" library from Brabander
+#include <LiquidCrystal_I2C.h>
+
+const int buttonPin = 3;        // GP3
+int number = 0;
+
+// Adres I2C (0x27 lub 0x3F)
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  Serial.begin(115200);
+  delay(5000);  // 5 sekund opóźnienia!
+
+  // Enable build in LED
+  pinMode(25, OUTPUT);
+  digitalWrite(25, HIGH);
+  
+  Serial.println("Inicjalizacja LCD...");
+  
+  lcd.init();                    
+  lcd.backlight();              
+  lcd.clear();                  // Wyczyść ekran
+  lcd.home();                   // Powrót do 0,0
+  lcd.print("Hej"); // Cała pierwsza linia
+  //lcd.setCursor(0, 1);          
+  //lcd.print("Testuje"); // Cała druga linia
+  
+  Serial.println("LCD gotowy!");
+
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop()
+{
+  lcd.clear();                  // Wyczyść ekran
+  lcd.print(number); // Cała druga linia
+  number++;
+  delay(1000);
+
+  if (digitalRead(buttonPin) == LOW)
+  {
+    number = 0;
+  }
+}
